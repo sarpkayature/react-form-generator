edb7dffe0c6980d06726aab9ca4b54ca56d71c5c
Sarpkaya Ture <sarpkayature@gmail.com>
2022-03-27 13:39:43 +0300

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,57 @@
 import { FormGenerator } from "./react-form-generator/Form/FormGenerator";
+import * as Yup from "yup";
 
 const App = () => {
-  const onSubmit = (data) => console.log(data);
+  const formProps = {
+    defaultValues: {
+      city: "",
+    },
+    onSubmit: (data) => console.log(data),
+    onClick: (data) => console.log(data),
+  };
+
+  const validationSchema = Yup.object().shape({
+    city: Yup.string().required("this field is required"),
+  });
+
   const formSchema = {
-    id: "",
-    fieldSets: {
-      rows: [
-        {
-          type: "text",
-          name: "city",
-          label: "Istanbul",
-          id: "city",
-          onSubmit: onSubmit,
-        },
-        {
-          type: "select",
-          name: "city",
-          label: "Istanbul",
-          id: "city",
-          onSubmit: onSubmit,
-        },
-      ],
+    // validation: validationSchema,
+    rows: [
+      {
+        key: "city",
+        name: "city",
+        type: "text",
+        label: "City",
+        size: "small",
+        variant: "outlined",
+        fullWidth: true,
+        margin: "normal",
+        gridSize: { sm: 12, md: 6, lg: 6 },
+      },
+    ],
+    btnProps: {
+      actionChild: "Kaydet",
+      closeChild: "Kapat",
+      btnType: "submit",
+      size: "small",
+      variant: "outlined",
+      btnGridSize: {
+        sm: 6,
+        md: 6,
+        lg: 4,
+      },
     },
   };
-  return <div className='App'></div>;
+
+  const form = {
+    formProps: formProps,
+    formSchema: formSchema,
+  };
+  return (
+    <div className='App'>
+      <FormGenerator {...form} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/formSchema.tsx
@@ -0,0 +1,82 @@
+// import * as yup from “yup”;
+// import CloseIcon from “@mui/icons-material/Close”;
+// import CheckIcon from “@mui/icons-material/Check”;
+// export const validationSchema = yup.object().shape({
+//   name: yup.string().required(“This Field is required”),
+// });
+// export const createFormSchema = {
+//   rows: [
+//     {
+//       key: “name”,
+//       name: “name”,
+//       type: “text”,
+//       label: “Isim”,
+//       size: “small”,
+//       variant: “outlined”,
+//       fullWidth: true,
+//       margin: “normal”,
+//       gridSize: { sm: 12, md: 6, lg: 6 },
+//     },
+//     {
+//       key: “type”,
+//       name: “type”,
+//       type: “text”,
+//       label: “Tip”,
+//       size: “small”,
+//       variant: “outlined”,
+//       fullWidth: true,
+//       margin: “normal”,
+//       gridSize: { sm: 12, md: 6, lg: 6 },
+//     },
+//     // {
+//     //   key: “options”,
+//     //   name: “options”,
+//     //   type: “select”,
+//     //   size: “small”,
+//     //   variant: “outlined”,
+//     //   fullWidth: true,
+//     //   label: “Seçenekler”,
+//     //   options: [
+//     //     { value: “1”, label: “Option 1" },
+//     //     { value: “2”, label: “Option 2" },
+//     //   ],
+//     //   gridSize: { sm: 12, md: 6, lg: 6 },
+//     // },
+//     {
+//       key: “isActive”,
+//       name: “isActive”,
+//       type: “checkbox”,
+//       label: “Aktif?“,
+//       color: “primary”,
+//       size: “small”,
+//       fullWidth: true,
+//       gridSize: { sm: 12, md: 6, lg: 6 },
+//     },
+//     // {
+//     //   key: “date”,
+//     //   name: “date”,
+//     //   type: “date”,
+//     //   label: “Tarih”,
+//     //   size: “small”,
+//     //   variant: “outlined”,
+//     //   fullWidth: true,
+//     //   margin: “normal”,
+//     //   gridSize: { sm: 12, md: 6, lg: 6 },
+//     // },
+//   ],
+//   btnProps: {
+//     actionChild: “Kaydet”,
+//     closeChild: “Kapat”,
+//     btnType: “submit”,
+//     size: “small”,
+//     variant: “outlined”,
+//     btnGridSize: {
+//       sm: 6,
+//       md: 6,
+//       lg: 4,
+//     },
+//     btnCloseIcon: <CloseIcon />,
+//     btnActionIcon: <CheckIcon />,
+//   },
+//   validationSchema: validationSchema,
+// };
